71bb1183df952f6e5eaad7ed80052a343e2deb61
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2025-04-28 18:00:33 +0200

    Add Spring dependencies

--- gui/src/main/java/module-info.java
+++ gui/src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module org.example.gui {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.net.http;
+    requires spring.webflux;
+    requires reactor.core;
 
 
     opens org.example.gui to javafx.fxml;
