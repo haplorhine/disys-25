70f50c2d47e3a6e181597f0b554f44eaaefeed04
Fritz Pick <36515150+haplorhine@users.noreply.github.com>
2025-05-04 18:37:41 +0200

    Change Scene height to auto

--- gui/src/main/java/org/example/gui/HelloApplication.java
+++ gui/src/main/java/org/example/gui/HelloApplication.java
@@ -3,6 +3,7 @@ package org.example.gui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +12,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 620, Region.USE_COMPUTED_SIZE);
         stage.setTitle("Hello Stefan and Philipp!");
         stage.setScene(scene);
         stage.show();
