4293a99fb13a61dfe6436dd649d8c420c9f1a3c9
Philipp <wi23b151@technikum-wien.at>
2025-06-22 16:43:17 +0200

    energy user mit echten daten angepasst

---
+++ energyuser/src/main/java/com/example/energyuser/data/User.java
@@ -0,0 +1,63 @@
+package com.example.energyuser.data;
+
+import java.time.LocalDateTime;
+
+public class User {
+
+    private String type;
+    private String association;
+    private double kwh;
+    private LocalDateTime datetime;
+
+    public User() {
+    }
+
+    public User(String type, String association, double kwh, LocalDateTime datetime) {
+        this.type = type;
+        this.association = association;
+        this.kwh = kwh;
+        this.datetime = datetime;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAssociation() {
+        return association;
+    }
+
+    public void setAssociation(String association) {
+        this.association = association;
+    }
+
+    public double getKwh() {
+        return kwh;
+    }
+
+    public void setKwh(double kwh) {
+        this.kwh = kwh;
+    }
+
+    public LocalDateTime getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(LocalDateTime datetime) {
+        this.datetime = datetime;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "type='" + type + '\'' +
+                ", association='" + association + '\'' +
+                ", kwh=" + kwh +
+                ", datetime=" + datetime +
+                '}';
+    }
+}
