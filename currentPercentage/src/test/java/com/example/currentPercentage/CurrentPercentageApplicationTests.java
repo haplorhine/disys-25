7fc9098cedc1babf0850f8b3a641d8cb9b2428ee
Philipp <wi23b151@technikum-wien.at>
2025-06-22 10:41:44 +0200

    Kommentar ergänzt in TimedMessenger

---
+++ currentPercentage/src/test/java/com/example/currentPercentage/CurrentPercentageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.currentPercentage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrentPercentageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
